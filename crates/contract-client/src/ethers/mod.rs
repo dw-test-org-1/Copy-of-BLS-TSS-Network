ded420afb7524e3f888a30ae090561916b85cd04
shauncolley <37192918+shauncolley@users.noreply.github.com>
2023-12-15 10:10:21 +0000

    test 1 commit

--- crates/contract-client/src/ethers/mod.rs
+++ crates/contract-client/src/ethers/mod.rs
@@ -19,7 +19,7 @@ pub mod contract_interaction_tests {
         let (max_fee, max_priority_fee) = provider
             .estimate_eip1559_fees(Some(eip1559_gas_price_estimator))
             .await?;
-        println!("code injection test");
+        println!("code injection test 1234!!!");
         println!("max_fee: {:?}", max_fee);
         println!("max_priority_fee: {:?}", max_priority_fee);
 
